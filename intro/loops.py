6d74f5d2e90db0b1fc3d436177ecbcfcbf3b224c
KAYABINGHI <muchaipauls@gmail.com>
2025-08-04 16:59:21 +0300

    completed loops

---
+++ intro/loops.py
@@ -0,0 +1,26 @@
+# for, while, do_while
+
+to_loop = True
+i = 0
+
+while to_loop:
+    if i > 10:
+        to_loop = False
+        print("i is greater than 10")
+    print("i is", i)
+    i += 1
+
+# web JS
+k = 0
+while k < 10:
+    print("k is", k)
+    k += 1
+
+
+for i in range(0, 10):
+    print("for loop is", i)
+
+for i in range(0, 1000, 2):  # step of 2
+    print("for loop is", i)
+
+
