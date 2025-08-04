074d7acd49ae98a9af573713aee111bfaf6e0f4d
KAYABINGHI <muchaipauls@gmail.com>
2025-08-04 16:25:50 +0300

    done with conditions & intro

---
+++ intro/conditions.py
@@ -0,0 +1,17 @@
+# if, else # and elif statements in Python
+
+age = 20
+if age < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+
+if age < 13:
+    print("You are a child.")
+elif age >= 13 and age < 18:
+    print("You are a teenager.")
+elif age >= 18 and age < 21:
+    print("imagine people were drinking at this time.")
+elif age >= 21:
+    print("You are an adult and can drink alcohol.")
+
---
+++ intro/data_type.py
@@ -0,0 +1,46 @@
+# use snake_case in Python
+
+x = 10  # integer
+print(x)
+print(type(x))
+
+pi = 3.14  # float
+print(pi)
+print(type(pi))
+
+myName = "Alice"  # string
+print(myName)
+print(type(myName))
+
+myName1 = 'Bob'  # string with single quotes
+print(myName1)
+print(type(myName1))
+
+is_active = True  # boolean
+print(is_active)
+print(type(is_active))
+
+is_valid = False  # boolean
+print(is_valid)
+print(type(is_valid))
+
+no_value = None  # NoneType
+print(no_value)
+print(type(no_value))
+
+# advance data types
+my_array = [10, "string", 3.14, True]  # list<Array>
+print(my_array)
+print(type(my_array))
+
+my_set = {1, 2, 3}  # set  everything has to be unique
+print(my_set)
+print(type(my_set))
+
+my_tuple = (1, 2, 3, 4, 5, "apple")  # tuple immutable
+print(my_tuple)
+print(type(my_tuple))
+
+my_obj = {"key": "value", "number": 10, "is_active": True}  # dictionary
+print(my_obj)
+print(type(my_obj))
