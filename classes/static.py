0b57b2d1e83bbd08d1a4caf124b16b9b2d21a1c2
KAYABINGHI <muchaipauls@gmail.com>
2025-08-08 11:09:49 +0300

    catchup

---
+++ classes/static.py
@@ -0,0 +1,66 @@
+
+static # static.py
+# This file contains a class definition for Human with properties and methods.      
+# It includes an initializer, a method to get the name, and a method to print the object's details.
+#Getter a property of: <name>:<name>    
+
+
+from datetime import datetime
+
+def write_file(f_name,txt):
+    with open(f_name,'a') as file:
+        file.write(f"{txt} \n")
+       
+
+class Human():
+
+    species="H.sapiens"
+    genus="Homo"
+    
+    def __init__(self,gender,name):
+        print("The initializer wass called")
+        self._gender=gender
+        self._name=name
+        if self._gender=="Male":
+            self._ribs=24
+            self._curse="Suffer"
+        else :
+          self._ribs=23
+          self._curse="Pain"
+
+    @property
+    def name(self):
+        now = datetime.now()
+        print("Curreent date and time",now)
+        write_file(f_name="log.txt",txt=f"At {now} got name from adam")
+        return self._name
+    
+    @name.setter
+    def name(self,new_name):
+        # data integrity
+        if not isinstance(new_name,str):
+            print("Failed to update name")
+            return
+        #new_name is astring
+        now = datetime.now()
+        print("Curreent date and time",now)
+        write_file(f_name="log.txt",txt=f"At {now} Name changed from {self._name} to {new_name}")
+        self._name=new_name
+        return new_name
+
+    def print_self(self):
+        print("----------------------")
+        print("name",self._name)
+        print("gender",self._gender)
+        print("ribs",self._ribs)
+        print("curse",self._curse)
+        print("---------------------")
+
+
+# adam=Human(name="adam",gender="Male") #object from a class
+adam=Human(name="adam",gender="Male")
+eve=Human(name="eve",gender="Female")
+
+print("adam species",adam.species)
+print("eve species",eve.species)
+print("class property",Human.species)
